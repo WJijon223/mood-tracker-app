85f30b82a250f5a7f835f5ad035e629bea369fd2
WJijon223 <williamjijon223@gmail.com>
2025-01-18 13:38:26 -0500

    finished creating and styling the input box

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -5,18 +5,35 @@ export function CreatePost() {
     <>
       <div className="create-post">
         <div className="post-inputs">
-          <div className="input-box">
-            <label htmlFor="post-title">Post Title: </label>
-            <input type="text" id="post-title" name="post-title" />
+          <div className="left-box">
+            <div className="input-box">
+              <label htmlFor="post-title">Post Title: </label>
+              <input type="text" id="post-title" name="post-title" />
+            </div>
+            <div className="input-box">
+              <label htmlFor="post-date">Post Date: </label>
+              <input type="date" id="post-date" name="post-date" />
+            </div>
+            <div className="input-box">
+              <label htmlFor="post-feeling">Feeling: </label>
+              <input type="text" />
+            </div>
           </div>
-          <div className="input-box">
-            <label htmlFor="post-date">Post Date: </label>
-            <input type="date" id="post-date" name="post-date" />
+          <div className="right-box">
+            <div className="input-box">
+              <label htmlFor="post-content">Post Content: </label>
+              <textarea
+                name="post-content"
+                id="post-content"
+                rows={8}
+              ></textarea>
+            </div>
           </div>
-          <div className="input-box">
-            <label htmlFor="post-feeling">Feeling: </label>
-            <input type="text" />
-          </div>
+        </div>
+        <div className="create-buttons">
+          <button>Post</button>
+          <button>Archive</button>
+          <button>Delete</button>
         </div>
       </div>
     </>
