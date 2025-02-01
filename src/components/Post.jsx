aa24651ab5ee14a37914ff86b8cd11cbad8eb443
WJijon223 <williamjijon223@gmail.com>
2025-01-31 22:34:55 -0500

    final commit 2.0: added functionality for post button on archived posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,13 @@
 import "../styles/Post.css";
 
 export function Post(props) {
-  const { entry, entryIndex, handleDeleteEntry, handleArchiveEntry } = props;
+  const {
+    entry,
+    entryIndex,
+    handleDeleteEntry,
+    handleArchiveEntry,
+    handlePostEntry,
+  } = props;
   return (
     <div className="user-post">
       <h2>{entry.title}</h2>
@@ -11,19 +17,28 @@ export function Post(props) {
       </div>
       <h4 className="post-content">{entry.content}</h4>
       <div className="post-buttons">
+        {!entry.posted && (
+          <button
+            className="post-btn"
+            onClick={() => handlePostEntry(entryIndex)}
+          >
+            Post
+          </button>
+        )}
         <button
           className="post-btn"
           onClick={() => handleDeleteEntry(entryIndex)}
         >
           Delete
         </button>
-        <button
-          className="post-btn"
-          onClick={() => handleArchiveEntry(entryIndex)}
-          disabled={!entry.posted}
-        >
-          Archive
-        </button>
+        {entry.posted && (
+          <button
+            className="post-btn"
+            onClick={() => handleArchiveEntry(entryIndex)}
+          >
+            Archive
+          </button>
+        )}
       </div>
     </div>
   );
